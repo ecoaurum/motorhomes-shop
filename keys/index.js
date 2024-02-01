9c255172c7745a35587f2a5334395c1116f4dd47
Alexander Sakaly <80649695+ecoaurum@users.noreply.github.com>
2024-02-01 13:22:09 +0200

    Update index.js

--- keys/index.js
+++ keys/index.js
@@ -2,5 +2,5 @@ module.exports = {
     MONGODB_URI: 'mongodb+srv://Alex:Kd73PkjwqPSeZQaTA@cluster0.b6ywq7o.mongodb.net/?retryWrites=true&w=majority',
     SESSION_SECRET: 'some-secret-value',
     EMAIL_FROM: 'test@gmail.com',
-    BASE_URL: 'http://localhost:3000'
+    BASE_URL: 'https://motorhomes-shop.vercel.app/'
 }
